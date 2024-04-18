3449dc98ad438154c4a52bb95e4726c803b5391d
Rudi <65447220+rudityasalam@users.noreply.github.com>
2024-04-18 14:04:22 +0700

    add files via upload
    first

---
+++ puasaJava/src/main/java/com/example/puasa/PuasaApplication.java
@@ -0,0 +1,13 @@
+package com.example.puasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuasaApplication.class, args);
+	}
+
+}
---
+++ puasaJava/src/test/java/com/example/puasa/PuasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.puasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
